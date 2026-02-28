8094d5e1d73c6b4353958c8a68477dfdc41aa83f
Nikolas351 <nikolasaro1@gmail.com>
2026-02-28 13:06:48 -0500

    Add files via upload

---
+++ Class Examples/class.cpp
@@ -0,0 +1,40 @@
+/*
+Name: Nikolas Aronov
+Course: CSCI 272
+Lesson Number: Lesson 4
+Code Topic: Copy Constructor Example
+Date: 02/27/2026
+*/
+
+#include <iostream>
+#include <Airplane>
+#include <Passport>
+using namespace std;
+
+
+int main()
+{
+	Airplane flight1;
+
+	flight1.openGate();
+
+	Passport person1;
+	person1.giveName();
+
+	return 0;
+
+}
+
+/*
+Reflection:
+1. What I understood:
+I understand how to make classes.
+2. Difficulties:
+I only had a problem remembering how to call the class.
+3. Research:
+ I searched through the lesson on how to do the stuff and the examples.
+4. AI Usage:
+ I didnt use chatgpt.
+5. What I learned:
+ I learned how to set different classes
+*/
